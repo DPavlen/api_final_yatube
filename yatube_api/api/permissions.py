b6a2858d0d1a4f0db067f148943a96f05a1706fe
Pavlen <pavlen@192.168.1.84>
2023-06-04 21:57:31 -0700

    Iteration2& del comments&import

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -2,7 +2,7 @@ from rest_framework import permissions
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
-
+    """Редактирование объекта, только владельцам объекта."""
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
                 or request.user.is_authenticated)
--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,8 +1,7 @@
 from django.urls import include, path
-# from rest_framework.routers import DefaultRouter
 from rest_framework import routers
 
-from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
+from api.views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 
 
 router_v1 = routers.DefaultRouter()
